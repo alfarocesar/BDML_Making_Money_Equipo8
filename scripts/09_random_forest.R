79faa502d5c7c26c598acd09e28fbc76923958b5
César Alfaro <cao.sps@gmail.com>
2025-05-27 21:12:07 -0600

    Update 09_random_forest.R

--- scripts/09_random_forest.R
+++ scripts/09_random_forest.R
@@ -298,9 +298,9 @@ model_info <- list(
   date_created = Sys.time()
 )
 
-saveRDS(model_info, "stores/models/random_forest_model_info.rds")
+# saveRDS(model_info, "stores/models/random_forest_model_info.rds")
 
-cat("Información del modelo guardada en: stores/models/random_forest_model_info.rds\n")
+# cat("Información del modelo guardada en: stores/models/random_forest_model_info.rds\n")
 
 ###########################################
 # 11. RESUMEN FINAL                      #
